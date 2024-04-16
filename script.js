c6383553d44d8c3783e1cf37591b8710b5d4de1a
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-16 08:26:26 +0500

    Completed

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert("Javascript file connected")
+let inputForm = document.querySelector("#tableForm");
+let input = document.querySelector("#default-search");
+let table = document.querySelector("#table");
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let tableOf = input.value;
+  table.innerHTML = "";
+
+  for (let i = 1; i <= 10; i++) {
+    // const message = `${tableOf} X ${i} = ${tableOf * i}`;
+
+    table.innerHTML += `    <tr class="border border-gray-400 ">
+        <td class="border border-gray-400 px-4 py-2">${tableOf}</td>
+        <td class="border border-gray-400 px-4 py-2">X</td>
+        <td class="border border-gray-400 px-4 py-2">${i}</td>
+        <td class="border border-gray-400 px-4 py-2">= </td>
+        <td class="border border-gray-400 px-4 py-2">${tableOf * i}</td>
+    </tr>`;
+  }
+});
